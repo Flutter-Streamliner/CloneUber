f46ceb78e2b2daaef7a95d105bf184dd3099cb01
thisisstreamliner <thisisstreamliner@gmail.com>
2020-12-07 12:17:37 +0200

    Connect firebase database android

---
+++ lib/screens/main_page.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Uber'),
+      ),
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            DatabaseReference dbRef =
+                FirebaseDatabase.instance.reference().child('Test');
+            dbRef.set('isConnected');
+          },
+          height: 50,
+          minWidth: 300,
+          color: Colors.green,
+          child: Text(
+            'Task Connection',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
