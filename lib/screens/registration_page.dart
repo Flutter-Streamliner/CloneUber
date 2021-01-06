6d3407c53cd7b117445fb1b10cbf98062c9f5553
thisisstreamliner <thisisstreamliner@gmail.com>
2021-01-06 17:08:19 +0200

    Adding User Info to Firebase Database. Add check connection

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,19 +1,31 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:uber_clone_app/screens/login_page.dart';
+import 'package:uber_clone_app/screens/main_page.dart';
 import 'package:uber_clone_app/widgets/confirm_button.dart';
 
-class RegistrationPage extends StatelessWidget {
+class RegistrationPage extends StatefulWidget {
   static const String routeName = 'register';
 
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
   final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
 
   final fullNameController = TextEditingController();
+
   final emailController = TextEditingController();
+
   final phoneController = TextEditingController();
+
   final passwordController = TextEditingController();
 
-  void showSnackBar(title, context) {
+  void showSnackBar(title) {
     final SnackBar snackBar = SnackBar(
       content: Text(
         title,
@@ -25,11 +37,39 @@ class RegistrationPage extends StatelessWidget {
   }
 
   void registerUser() async {
-    auth.UserCredential credential = await _auth.createUserWithEmailAndPassword(
-        email: emailController.text, password: passwordController.text);
+    auth.UserCredential credential = await _auth
+        .createUserWithEmailAndPassword(
+            email: emailController.text, password: passwordController.text)
+        .catchError((error) {
+      // check error and display message
+      PlatformException exception = error;
+      showSnackBar(exception.message);
+    });
     if (credential.user != null) {
       print('Registration successful');
+      DatabaseReference newUserRef = FirebaseDatabase.instance
+          .reference()
+          .child('users/${credential.user.uid}');
+      // Prepare data to be saved on user table
+      Map userMap = {
+        'fullName': fullNameController.text,
+        'email': emailController.text,
+        'phone': phoneController.text,
+      };
+      newUserRef.set(userMap);
+      // Take the user to main page
+      Navigator.pushNamedAndRemoveUntil(
+          context, MainPage.routeName, (route) => false);
     }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    fullNameController.dispose();
+    emailController.dispose();
+    phoneController.dispose();
+    passwordController.dispose();
   }
 
   @override
@@ -137,34 +177,37 @@ class RegistrationPage extends StatelessWidget {
                       ),
                       ConfirmButton(
                         title: 'REGISTER',
-                        onPressed: () {
+                        onPressed: () async {
                           // check network availability
+                          var connectivityResult =
+                              await Connectivity().checkConnectivity();
+                          if (connectivityResult != ConnectivityResult.mobile &&
+                              connectivityResult != ConnectivityResult.wifi) {
+                            showSnackBar('No internet');
+                            return;
+                          }
 
                           if (fullNameController.text.trim().length < 3) {
-                            showSnackBar(
-                                'Please provide a valid full name', context);
+                            showSnackBar('Please provide a valid full name');
                             return;
                           }
                           if (!emailController.text.contains('@')) {
                             showSnackBar(
-                                'Please provide a correct email address',
-                                context);
+                                'Please provide a correct email address');
                             return;
                           }
                           if (emailController.text.trim().length < 5) {
-                            showSnackBar('Please provide a valid email address',
-                                context);
+                            showSnackBar(
+                                'Please provide a valid email address');
                             return;
                           }
                           if (phoneController.text.trim().length < 10) {
-                            showSnackBar(
-                                'Please provide a valid phone number', context);
+                            showSnackBar('Please provide a valid phone number');
                             return;
                           }
                           if (passwordController.text.trim().length < 8) {
                             showSnackBar(
-                                'Password must be at least 8 characters',
-                                context);
+                                'Password must be at least 8 characters');
                             return;
                           }
                           registerUser();
