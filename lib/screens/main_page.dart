02dde33de9d34cbd8b6e31a1c7ec3f57aec3c826
thisisstreamliner <thisisstreamliner@gmail.com>
2021-01-12 11:08:20 +0200

    Add search panel

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -22,12 +22,81 @@ class _MainPageState extends State<MainPage> {
         body: Stack(
       children: [
         GoogleMap(
-          mapType: MapType.hybrid,
+          mapType: MapType.normal,
           myLocationButtonEnabled: true,
           initialCameraPosition: _kGooglePlex,
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Container(
+            height: 240,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 15,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  ),
+                ]),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 24, vertical: 18.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text('Nice to see you!', style: TextStyle(fontSize: 10)),
+                  Text(
+                    'Where are you going?',
+                    style: TextStyle(fontSize: 18, fontFamily: 'Brand-Bold'),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(4),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5.0,
+                            spreadRadius: 0.5,
+                            offset: Offset(0.7, 0.7),
+                          ),
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.search,
+                            color: Colors.blueAccent,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('Search Destination'),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         )
       ],
     ));
