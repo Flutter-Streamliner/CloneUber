d09a36db7526154d86fbc0b8496ec0809d5fa33d
Tiger <thisisstreamliner@gmail.com>
2021-03-04 13:14:34 +0200

    Fitting polyline on Map using LatLng Bounds

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -409,5 +409,28 @@ class _MainPageState extends State<MainPage> {
     setState(() {
       _polylines.add(polyline);
     });
+
+    // make polyline to fit into the map
+    LatLngBounds bounds;
+    if (pickLatLng.latitude > destinationLatLng.latitude &&
+        pickLatLng.longitude > destinationLatLng.longitude) {
+      bounds =
+          LatLngBounds(southwest: destinationLatLng, northeast: pickLatLng);
+    } else if (pickLatLng.longitude > destinationLatLng.longitude) {
+      bounds = LatLngBounds(
+        southwest: LatLng(pickLatLng.latitude, destinationLatLng.longitude),
+        northeast: LatLng(destinationLatLng.latitude, pickLatLng.longitude),
+      );
+    } else if (pickLatLng.latitude > destinationLatLng.latitude) {
+      bounds = LatLngBounds(
+        southwest: LatLng(destinationLatLng.latitude, pickLatLng.longitude),
+        northeast: LatLng(pickLatLng.latitude, destinationLatLng.longitude),
+      );
+    } else {
+      bounds =
+          LatLngBounds(southwest: pickLatLng, northeast: destinationLatLng);
+    }
+
+    _mapController.animateCamera(CameraUpdate.newLatLngBounds(bounds, 70));
   }
 }
